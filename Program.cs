223b9193d8c5e299a6fb46f1ddcc6d4e90e07f7d
Nguyễn Đình Hải <93904326+SaberValkyrie@users.noreply.github.com>
2022-12-23 16:42:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+class Gender
+
+{
+    static void Main()
+    {
+        Gender male = new Male();
+        Gender female = new Female();
+        male.check();
+        female.check();
+    }
+    public virtual void check() { Console.WriteLine("Check Gender"); }
+}
+class Male : Gender
+{ public override void check() { Console.WriteLine("Gender:Male"); } }
+class Female : Gender
+{ public override void check() { Console.WriteLine("Gender:Female"); } }
